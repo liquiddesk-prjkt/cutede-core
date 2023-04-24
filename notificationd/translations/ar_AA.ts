3af5b96c08c576a675a33a7ec2d49f37cad38034
UsiFX <xprjkts@gmail.com>
2023-04-24 20:17:45 +0200

    notificationd: translations: ar_AA: implement translation
    Signed-off-by: UsiFX <xprjkts@gmail.com>

---
+++ notificationd/translations/ar_AA.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>DateHelper</name>
+    <message>
+        <location filename="../datehelper.cpp" line="15"/>
+        <source>حالا</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datehelper.cpp" line="17"/>
+        <source>منذ دقيقة</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datehelper.cpp" line="19"/>
+        <source>%1 منذ دقيقة</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datehelper.cpp" line="23"/>
+        <source>منذ ساعة</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datehelper.cpp" line="25"/>
+        <source>%1 منذ ساعة</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datehelper.cpp" line="29"/>
+        <source>1 منذ يوم</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datehelper.cpp" line="31"/>
+        <source>%1 منذ ايام مضت</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationWindow</name>
+    <message>
+        <location filename="../qml/NotificationWindow.qml" line="91"/>
+        <source>مركز الإشعارات</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/NotificationWindow.qml" line="123"/>
+        <source>لا توجد إشعارات</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
